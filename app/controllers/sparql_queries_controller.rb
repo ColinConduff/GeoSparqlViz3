3d24beb403f54ecc51d6d6bc502e6ef2bd2c8444
Colin Conduff <cconduff098@gmail.com>
2016-04-12 13:51:45 -0500

    Achieve different functionality for index and show pages.

--- app/controllers/sparql_queries_controller.rb
+++ app/controllers/sparql_queries_controller.rb
@@ -1,7 +1,7 @@
 class SparqlQueriesController < ApplicationController
 
   before_action :authenticate_user!
-	before_action :all_sparql_queries, only: [:index, :show, :edit, :new, :create, :update, :destroy]
+	before_action :all_sparql_queries, only: [:index, :edit, :new, :create, :update, :destroy]
   before_action :set_sparql_queries, only: [:edit, :update, :destroy]
   before_action :get_data_for_form, only: [:new, :edit]
   respond_to :html, :js
@@ -9,8 +9,15 @@ class SparqlQueriesController < ApplicationController
   # update this to only show root sparql queries
   # .where('parent_query_id = nil/null')
   def index
-    @other_user_queries = SparqlQuery.joins(:user).uniq.where('user_id != ?', current_user )
-    @current_user_queries = SparqlQuery.joins(:user).uniq.where('user_id = ?', current_user )
+    # @other_user_queries = SparqlQuery.joins(:user).uniq.where('user_id != ?', current_user )
+    # @current_user_queries = SparqlQuery.joins(:user).uniq.where('user_id = ?', current_user )
+    @this_is_the_index_page = true
+  end
+
+  def show
+    # @sparql_queries = SparqlQuery.joins(:user).uniq.where('user_id = ?', current_user )
+    @sparql_queries = find_all_child_queries(params[:id])
+    @this_is_the_index_page = false
   end
 
   def new
@@ -54,6 +61,19 @@ class SparqlQueriesController < ApplicationController
       
       # adding and id = ? breaks form
       # @parent_queries = SparqlQuery.joins(:user).uniq.where('user_id = ? AND id = ?', current_user, sparql_query.id )
+    end
+
+    def find_all_child_queries(parent_id)
+      child_queries = []
+      child_queries << SparqlQuery.find(parent_id)
+
+      direct_child_queries = SparqlQuery.where(parent_query_id: parent_id) 
+      direct_child_queries.each do |query|
+        child_queries.concat(find_all_child_queries(query.id))
+      end
+
+      # return child_queries array
+      child_queries 
     end
 
     def query_params
