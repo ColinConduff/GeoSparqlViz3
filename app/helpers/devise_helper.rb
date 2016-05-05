f64e3024d93431ce05da08720034f17b390bade0
ColinConduff <cconduff098@gmail.com>
2016-05-05 18:30:47 -0500

    Customize devise error messages and add error messages to log in

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,24 @@
+module DeviseHelper
+  def devise_error_messages!
+    return "" unless devise_error_messages?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:p, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      :count => resource.errors.count,
+                      :resource => resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div class="alert alert-danger">
+      <h4>#{sentence}</h4>
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def devise_error_messages?
+    !resource.errors.empty?
+  end
+
+end
